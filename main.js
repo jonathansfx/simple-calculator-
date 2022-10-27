2939e2981bb68c412c8395feef3e0942dc689800
Jonathan Luo <97814319+zombie0000@users.noreply.github.com>
2022-10-27 11:58:17 -0600

    zxc

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// JavaScript Code
+// SIMPLE ADDITION IPO CALCULATOR BY JONATHAN
+
+//Input
+let number1 = +prompt("Enter a number:");
+let number2 = +prompt("Enter another number");
+let number3 = +prompt("Enter a last number:");
+//Process
+let total = (number1 + number2) * number3;
+
+//Output
+alert(`The result is:(${number1} + ${number2}) * ${number3} = ${total}`);
